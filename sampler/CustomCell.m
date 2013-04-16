1c583e4e3e6463cb564dd0e3fdee7dfd1acaef3e
Ivo von Putzer <ivo.putzer@gmail.com>
2013-04-16 12:48:51 +0200

    2

--- sampler/CustomCell.m
+++ sampler/CustomCell.m
@@ -13,8 +13,9 @@
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+    if (self)
+    {
+
     }
     return self;
 }
@@ -22,8 +23,6 @@
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
