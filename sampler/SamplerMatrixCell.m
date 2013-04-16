7afc4c5ee25e7ab2f375aa62e527cade49e9bc17
Ivo von Putzer <ivo.putzer@gmail.com>
2013-04-16 11:58:34 +0200

    Fix

--- sampler/SamplerMatrixCell.m
+++ sampler/SamplerMatrixCell.m
@@ -10,8 +10,6 @@
 
 @interface SamplerMatrixCell()
 
-@property (weak, nonatomic) IBOutlet UIImageView *image;
-
 @property (weak, nonatomic) IBOutlet UIButton *button;
 
 @property BOOL state;
@@ -28,24 +26,21 @@
 
 -(void) color: (UIColor*) color
 {
-    [self.image setAlpha:1];
+    // [self.image setAlpha:1];
 }
 
 /* --- Action Events --- */
 
 - (IBAction)click:(UIButton*)button
 {
-    [button setBackgroundColor:[UIColor colorWithRed:35.0f/255.0f green:31.0f/255.0f blue:32.0f/255.0f alpha:1.0]];
+    UIColor *color = self.state
+        ? [UIColor colorWithRed:0.0f/255.0f green:187.0f/255.0f blue:226.0f/255.0f alpha:1.0]
+        : [UIColor colorWithRed:35.0f/255.0f green:31.0f/255.0f blue:32.0f/255.0f alpha:1.0];
     
     
-    /*if(tones[[button getI]][[button getJ]]){
-     
-
-    }
-    else {
-        [button setBackgroundColor:[UIColor colorWithRed:0.0f/255.0f green:187.0f/255.0f blue:226.0f/255.0f alpha:1.0]];
-    }
-    tones[[button getI]][[button getJ]] = tones[[button getI]][[button getJ]] ? false : true;*/
+    [button setBackgroundColor: color];
+        
+    self.state = !self.state; // toggler
 }
 
 /*
