7e44273d58d5636c6c1b851ad1c59c638012ce7d
Niko Santoro <djsantoroniko@yahoo.it>
2013-04-17 12:01:44 +0200

    Insert feature Voice Recorder; ---------------------------------------- Insert icons: -Play -Pause -Rec -Stop

--- sampler/ViewController.h
+++ sampler/ViewController.h
@@ -9,6 +9,20 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController <UITableViewDelegate>
+@interface ViewController : UIViewController <UITableViewDelegate, AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+{
+    AVAudioPlayer *audioPlayer;
+    AVAudioRecorder *audioRecorder;
+    int recordEncoding;
+    enum
+    {
+        ENC_AAC = 1,
+        ENC_ALAC = 2,
+        ENC_IMA4 = 3,
+        ENC_ILBC = 4,
+        ENC_ULAW = 5,
+        ENC_PCM = 6,
+    } encodingTypes;
+}
 
 @end
