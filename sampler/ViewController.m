67c354b94d0a84badf2e9e2e94a2de7d94737831
Ivo von Putzer <ivo.putzer@gmail.com>
2013-04-18 14:46:14 +0200

    EXTREME REFACTORING

--- sampler/ViewController.m
+++ sampler/ViewController.m
@@ -14,16 +14,17 @@
 
 @interface ViewController ()
 
-@property (strong, nonatomic) NSMutableArray* objects;
-@property (strong, nonatomic) NSMutableArray* bundles;
-@property (strong, nonatomic) NSMutableArray* instruments;
-@property (weak, nonatomic) IBOutlet UIButton *playButton;
-@property (weak, nonatomic) IBOutlet UIButton *recButton;
 @property (weak, nonatomic) SamplerMatrixViewController* sampleMatrixViewController;
-@property (weak, nonatomic) IBOutlet UILabel *bundleLabel;
 
-@property (weak, nonatomic) IBOutlet UITableView *previewTable;
-@property (weak, nonatomic) IBOutlet UITableView *KitTable;
+@property (strong, nonatomic) NSMutableArray* bundles;
+
+@property (strong, nonatomic) NSMutableArray* samples;
+
+@property (weak, nonatomic) IBOutlet UITableView *samplesTable;
+
+@property (weak, nonatomic) IBOutlet UITableView *bundlesTable;
+
+@property (weak, nonatomic) IBOutlet UILabel *bundleName;
 
 @end
 
@@ -35,13 +36,7 @@
 {
     [super viewDidLoad];
     
-    self.navigationController.navigationBar.tintColor = [UIColor grayColor];
-    
-    [self setObjects: [[NSMutableArray alloc] initWithArray:@[
-                       
-       @{ @"track": @"draftpunk", @"dirname" : @"draftpunk" }
-    
-    ]]];     
+    [self.navigationController.navigationBar setTintColor: [UIColor grayColor]];
      
     [self setBundles: [[NSMutableArray alloc] initWithArray:@[
         @{
@@ -71,169 +66,24 @@
             ]
         }
     ]]];
+
     
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];    
-    [defaults setObject:self.bundles forKey:@"bundles"];
-    
+    // nice to have everywhere
+    [[NSUserDefaults standardUserDefaults] setObject:self.bundles forKey:@"bundles"];
 }
 
--(void)showInstrumentTable
-{
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSLog(@"bundleSelected: %d", [defaults integerForKey:@"bundle"]);
-    
-    self.instruments = [[NSMutableArray alloc]initWithArray:[[[defaults objectForKey:@"bundles"] objectAtIndex:[defaults integerForKey:@"bundle"]]  objectForKey:@"files"]];
-    
-    self.bundleLabel.text = self.bundles[[defaults integerForKey:@"bundle"]][@"bundle"];
-    [self.previewTable reloadData];
-    //NSLog(@"SELF.INSTRUMENT: %@", self.instruments);
-}
 
-- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
-{
-    [self.playButton setImage:[UIImage imageNamed:@"play.png"] forState:UIControlStateNormal];
-}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView { return 1; }
 
-- (IBAction)startRecording:(id)sender {
-    
-    BOOL animation = [sender tag];
-    
-    [sender setTag:![sender tag]];
-    
-    if(!animation){
-        
-        [self.recButton setImage:[UIImage imageNamed:@"stop.png"] forState:UIControlStateNormal];
-        
-        NSLog(@"startRecording");
-        audioRecorder = nil;
-        
-        // Init audio with record capability
-        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        [audioSession setCategory:AVAudioSessionCategoryRecord error:nil];
-        
-        NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] initWithCapacity:10];
-        if(recordEncoding == ENC_PCM)
-        {
-            [recordSettings setObject:[NSNumber numberWithInt: kAudioFormatLinearPCM] forKey: AVFormatIDKey];
-            [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
-            [recordSettings setObject:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
-            [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-            [recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
-            [recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
-        }
-        else
-        {
-            NSNumber *formatObject;
-            
-            switch (recordEncoding) {
-                case (ENC_AAC):
-                    formatObject = [NSNumber numberWithInt: kAudioFormatMPEG4AAC];
-                    break;
-                case (ENC_ALAC):
-                    formatObject = [NSNumber numberWithInt: kAudioFormatAppleLossless];
-                    break;
-                case (ENC_IMA4):
-                    formatObject = [NSNumber numberWithInt: kAudioFormatAppleIMA4];
-                    break;
-                case (ENC_ILBC):
-                    formatObject = [NSNumber numberWithInt: kAudioFormatiLBC];
-                    break;
-                case (ENC_ULAW):
-                    formatObject = [NSNumber numberWithInt: kAudioFormatULaw];
-                    break;
-                default:
-                    formatObject = [NSNumber numberWithInt: kAudioFormatAppleIMA4];
-            }
-            
-            [recordSettings setObject:formatObject forKey: AVFormatIDKey];
-            [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
-            [recordSettings setObject:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
-            [recordSettings setObject:[NSNumber numberWithInt:12800] forKey:AVEncoderBitRateKey];
-            [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-            [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityHigh] forKey: AVEncoderAudioQualityKey];
-        }
-        
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString* documentDirectory = paths[0];
-        
-        NSString* databaseFile = [documentDirectory stringByAppendingPathComponent:@"provaRec"];
-        
-        NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@", databaseFile]];
-        
-        
-        NSError *error = nil;
-        audioRecorder = [[ AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:&error];
-        
-        if ([audioRecorder prepareToRecord] == YES){
-            [audioRecorder record];
-        }else {
-            int errorCode = CFSwapInt32HostToBig ([error code]);
-            NSLog(@"Error: %@ [%4.4s])" , [error localizedDescription], (char*)&errorCode);
-            
-        }
-        NSLog(@"recording");
-    }else{
-        [self.recButton setImage:[UIImage imageNamed:@"rec.png"] forState:UIControlStateNormal];
-        NSLog(@"stopRecording");
-        [audioRecorder stop];
-        NSLog(@"stopped");
-    }
-    
-}
-
-- (IBAction)playRecording:(id)sender {
-    
-    BOOL animation = [sender tag];
-    
-    [sender setTag:![sender tag]];
-    
-    if(!animation){
-        
-        [self.playButton setImage:[UIImage imageNamed:@"pause.png"] forState:UIControlStateNormal];
-        
-        NSLog(@"playRecording");
-        // Init audio with playback capability
-        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
-        
-        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString* documentDirectory = paths[0];
-        
-        NSString* databaseFile = [documentDirectory stringByAppendingPathComponent:@"provaRec"];
-        
-        NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@", databaseFile]];
-        NSError *error;
-        audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
-        
-        [audioPlayer addObserver:self forKeyPath:@"currentTime" options:NSKeyValueObservingOptionNew context:nil];
-        
-        [audioPlayer setDelegate:self];
-        NSLog(@"Duration: %f", audioPlayer.duration);
-        audioPlayer.numberOfLoops = 0;
-        [audioPlayer play];
-        NSLog(@"playing");
-    }else{
-        [self.playButton setImage:[UIImage imageNamed:@"play.png"] forState:UIControlStateNormal];
-        [audioPlayer pause];
-    }
-    
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-/*COMMIT*/
 
 - (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section
 {
-    return (self.KitTable == table) ? self.bundles.count : self.instruments.count;
+    return (self.bundlesTable == table) ? self.bundles.count : self.samples.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)table cellForRowAtIndexPath:(NSIndexPath *)index
 {
-    if ( self.KitTable == table ) // the one on the left
+    if ( self.bundlesTable == table ) // the one on the left
     {        
         CustomCell *cell = [table dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:index];
         
@@ -243,81 +93,160 @@
     {     
         BundleTableCell *cell = [table dequeueReusableCellWithIdentifier:@"BundleTableCell" forIndexPath:index];
                 
-        return [cell withInfo:self.instruments[index.row]];        
+        return [cell withInfo:self.samples[index.row]];
     }
 }
 
--(void)cellClickedWithTag:(int)tag andUrl:(NSString *)typeOfInstrument
+
+- (void)tableView:(UITableView *)table didSelectRowAtIndexPath:(NSIndexPath *)index
 {
-    NSLog(@"Cella cliccata con TAG: %d, con il corrispondente URL della traccia: %@",tag,typeOfInstrument);
+    if ( self.bundlesTable == table ) [self selectBundle: index.row];
 }
 
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+-(void) selectBundle: (int) selection
 {
+    [self setSamples: [[NSMutableArray alloc] initWithArray: self.bundles[selection][@"files"]]];
     
-}
-
--(void)showAlert:(NSDictionary*)element
-{
-    //UIAlertView *alert = [[UIAlertView alloc]initWithTitle:element[@"name"] message:@"Ciao" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+    [self setBundleName: self.bundles[selection][@"bundle"]];
     
-    //[alert show];
+    [self.bundlesTable reloadData];
 }
 
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (tableView == self.KitTable) {
-        NSLog(@"La cella %d è stata cliccata", indexPath.row );
-        
-        [self showAlert:self.bundles[indexPath.row]];
-        
-        NSUserDefaults *prefBundleSelected = [NSUserDefaults standardUserDefaults];
-        [prefBundleSelected setInteger:indexPath.row forKey:@"bundle"];
-        
-        [self showInstrumentTable];
-    }
-    
-}
 
+/*
+ 
+------------------------- OLD STUFF WE USED FOR RECORDING THE MICROPHONE -------------------------
+ 
+ 
+ - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+ {
+ [self.playButton setImage:[UIImage imageNamed:@"play.png"] forState:UIControlStateNormal];
+ }
+ 
+ - (IBAction)startRecording:(id)sender {
+ 
+ BOOL animation = [sender tag];
+ 
+ [sender setTag:![sender tag]];
+ 
+ if(!animation){
+ 
+ [self.recButton setImage:[UIImage imageNamed:@"stop.png"] forState:UIControlStateNormal];
+ 
+ NSLog(@"startRecording");
+ audioRecorder = nil;
+ 
+ // Init audio with record capability
+ AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+ [audioSession setCategory:AVAudioSessionCategoryRecord error:nil];
+ 
+ NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] initWithCapacity:10];
+ if(recordEncoding == ENC_PCM)
+ {
+ [recordSettings setObject:[NSNumber numberWithInt: kAudioFormatLinearPCM] forKey: AVFormatIDKey];
+ [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
+ [recordSettings setObject:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+ [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+ [recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
+ [recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
+ }
+ else
+ {
+ NSNumber *formatObject;
+ 
+ switch (recordEncoding) {
+ case (ENC_AAC):
+ formatObject = [NSNumber numberWithInt: kAudioFormatMPEG4AAC];
+ break;
+ case (ENC_ALAC):
+ formatObject = [NSNumber numberWithInt: kAudioFormatAppleLossless];
+ break;
+ case (ENC_IMA4):
+ formatObject = [NSNumber numberWithInt: kAudioFormatAppleIMA4];
+ break;
+ case (ENC_ILBC):
+ formatObject = [NSNumber numberWithInt: kAudioFormatiLBC];
+ break;
+ case (ENC_ULAW):
+ formatObject = [NSNumber numberWithInt: kAudioFormatULaw];
+ break;
+ default:
+ formatObject = [NSNumber numberWithInt: kAudioFormatAppleIMA4];
+ }
+ 
+ [recordSettings setObject:formatObject forKey: AVFormatIDKey];
+ [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
+ [recordSettings setObject:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+ [recordSettings setObject:[NSNumber numberWithInt:12800] forKey:AVEncoderBitRateKey];
+ [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+ [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityHigh] forKey: AVEncoderAudioQualityKey];
+ }
+ 
+ NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+ NSString* documentDirectory = paths[0];
+ 
+ NSString* databaseFile = [documentDirectory stringByAppendingPathComponent:@"provaRec"];
+ 
+ NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@", databaseFile]];
+ 
+ 
+ NSError *error = nil;
+ audioRecorder = [[ AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:&error];
+ 
+ if ([audioRecorder prepareToRecord] == YES){
+ [audioRecorder record];
+ }else {
+ int errorCode = CFSwapInt32HostToBig ([error code]);
+ NSLog(@"Error: %@ [%4.4s])" , [error localizedDescription], (char*)&errorCode);
+ 
+ }
+ NSLog(@"recording");
+ }else{
+ [self.recButton setImage:[UIImage imageNamed:@"rec.png"] forState:UIControlStateNormal];
+ NSLog(@"stopRecording");
+ [audioRecorder stop];
+ NSLog(@"stopped");
+ }
+ 
+ }
+ 
+ - (IBAction)playRecording:(id)sender {
+ 
+ BOOL animation = [sender tag];
+ 
+ [sender setTag:![sender tag]];
+ 
+ if(!animation){
+ 
+ [self.playButton setImage:[UIImage imageNamed:@"pause.png"] forState:UIControlStateNormal];
+ 
+ NSLog(@"playRecording");
+ // Init audio with playback capability
+ AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+ [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
+ 
+ NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+ NSString* documentDirectory = paths[0];
+ 
+ NSString* databaseFile = [documentDirectory stringByAppendingPathComponent:@"provaRec"];
+ 
+ NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@", databaseFile]];
+ NSError *error;
+ audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+ 
+ [audioPlayer addObserver:self forKeyPath:@"currentTime" options:NSKeyValueObservingOptionNew context:nil];
+ 
+ [audioPlayer setDelegate:self];
+ NSLog(@"Duration: %f", audioPlayer.duration);
+ audioPlayer.numberOfLoops = 0;
+ [audioPlayer play];
+ NSLog(@"playing");
+ }else{
+ [self.playButton setImage:[UIImage imageNamed:@"play.png"] forState:UIControlStateNormal];
+ [audioPlayer pause];
+ }
+ 
+ }*/
 
 @end
